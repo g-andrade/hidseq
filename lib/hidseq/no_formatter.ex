feb34e844d6e52909689a2ae7d7267346db1f6ef
Guilherme Andrade <g@gandrade.net>
2023-09-10 16:56:39 +0100

    Allow for unformatted output

---
+++ lib/hidseq/no_formatter.ex
@@ -0,0 +1,34 @@
+defmodule HidSeq.NoFormatter do
+  alias HidSeq.Formatter
+
+  @enforce_keys []
+  defstruct []
+
+  @opaque t :: %__MODULE__{}
+
+  ## API
+
+  def new!() do
+    %__MODULE__{}
+  end
+
+  ## Formatter
+
+  defimpl Formatter, for: __MODULE__ do
+    alias HidSeq.NoFormatter
+
+    ## API
+
+    def encode!(%NoFormatter{}, integer) do
+      integer
+    end
+
+    def decode(%NoFormatter{}, value) do
+      if is_integer(value) and value >= 0 do
+        {:ok, value}
+      else
+        {:error, {:not_a_non_negative_integer, value}}
+      end
+    end
+  end
+end
